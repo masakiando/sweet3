25b0871efc5142c06c07b5c7bdf97e450c4c5c2f
Your Name <you@example.com>
2017-01-16 19:37:07 +0900

    仮説top page実装。

---
+++ app/controllers/worker/top_controller.rb
@@ -0,0 +1,5 @@
+class Worker::TopController < ApplicationController
+  def index
+    render action: 'index'
+  end
+end
--- command.rb
+++ command.rb
@@ -1,2 +1,6 @@
 bin/bundle exec rake db:setup #ENV[dev, test]debTB作成、schema&seeds読み込み
 bin/bundle exec rake db:setup RAILS_ENV=production#TB作成、schema&seeds読み込み
+
+bin/rails g controller admin/top index  
+bin/rails g controller staff/top index 
+bin/rails g controller worker/top index                          
